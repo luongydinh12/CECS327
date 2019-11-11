c2c99e51a01c3e3034d3d258c6509bb341842e1f
Dinh Luong <luongydinh12@gmail.com>
2019-11-10 20:19:10 -0800

    socket programming - user inputs

--- Socket Programming/UDPClient.java
+++ Socket Programming/UDPClient.java
@@ -1,21 +1,34 @@
 import java.net.*;
 import java.io.*;
-
+import java.io.Console;
 public class UDPClient {
     public static void main(String args[]) {
         // args give message contents and server hostname
+        System.out.println("This is a Client");
+        System.out.println("Please enter the IP Address provided by Server:");
+        Console console = System.console();
+        String IPAddress = console.readLine();     
+        System.out.println("Please enter the port number provided by Server:");
+        int serverPort = Integer.parseInt(console.readLine());
+
         DatagramSocket aSocket = null;
+        int cont = 1;
         try {
             aSocket = new DatagramSocket();
-            byte[] m = args[0].getBytes();
-            InetAddress aHost = InetAddress.getByName(args[1]);
-            int serverPort = 6789;
+            while(cont != 0){
+            System.out.println("Please enter your message:");
+            String msg = console.readLine();
+            byte[] m = msg.getBytes();
+            InetAddress aHost = InetAddress.getByName(IPAddress);
             DatagramPacket request = new DatagramPacket(m, m.length, aHost, serverPort);
             aSocket.send(request);
             byte[] buffer = new byte[1000];
             DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
             aSocket.receive(reply);
             System.out.println("Reply: " + new String(reply.getData()));
+            System.out.println("Do you want to continue? (input 0 for Yes, other number for No) : ");
+            cont = Integer.parseInt(console.readLine());
+        }
         } catch (SocketException e) {
             System.out.println("Socket: " + e.getMessage());
         } catch (IOException e) {
--- Socket Programming/UDPServer.java
+++ Socket Programming/UDPServer.java
@@ -1,21 +1,35 @@
 import java.net.*;
 import java.io.*;
-
+import java.net.InetAddress; 
+import java.io.Console;
 public class UDPServer {
     public static void main(String args[]) {
+
         DatagramSocket aSocket = null;
+        String host;
         try {
-            aSocket = new DatagramSocket(6789);
-            byte[] buffer = new byte[1000];
-			int i = 0;
+            host = InetAddress.getLocalHost().getHostAddress();
+          }
+          catch (UnknownHostException ex) {
+            host = "unknown";
+          }
+        System.out.println("This is a Server");
+        System.out.println("Server IP Address : " +  host); 
+        System.out.println("Please enter a port number:");
+        Console console = System.console();
+        int portNumber = Integer.parseInt(console.readLine());
+        int i = 0;
+        try {
+            aSocket = new DatagramSocket(portNumber);
+            byte[] buffer = new byte[1000];      
             while (true) {
                 DatagramPacket request = new DatagramPacket(buffer, buffer.length);
                 aSocket.receive(request);
                 DatagramPacket reply = new DatagramPacket(request.getData(), request.getLength(), request.getAddress(),
                         request.getPort());
                 aSocket.send(reply);
-				i++;
-				System.out.println("Finished " +i+" jobs");
+                i++;
+                System.out.println("Message(s) processed : "+i);
             }
         } catch (SocketException e) {
             System.out.println("Socket: " + e.getMessage());
