59439ead70b4aab870f48d134f787c1696ffc9c9
Dinh Luong <luongydinh12@gmail.com>
2019-11-23 13:49:13 -0800

    socket programming updated

--- Socket Programming/UDPClient.java
+++ Socket Programming/UDPClient.java
@@ -1,34 +1,50 @@
+// Group 5: Tyler Ton, Dinh Luong
+// CECS 327 - Assignment 3
+// This is an UDP Client code provided by instructor
+// Modified by Dinh Luong and Tyler Ton
+
 import java.net.*;
 import java.io.*;
 import java.io.Console;
+
 public class UDPClient {
+
     public static void main(String args[]) {
-        // args give message contents and server hostname
-        System.out.println("This is a Client");
-        System.out.println("Please enter the IP Address provided by Server:");
+        // Show user this is a Client
+        System.out.println("\nThis is an Echo Client");
         Console console = System.console();
-        String IPAddress = console.readLine();     
-        System.out.println("Please enter the port number provided by Server:");
-        int serverPort = Integer.parseInt(console.readLine());
+	// Get the IP Address from user by calling checkIpAddress method
+	String IPAddress = checkIpAddress();
+	// Get the port number from user by calling checkPortNumber method
+	int serverPort = checkPortNumber();
 
         DatagramSocket aSocket = null;
-        int cont = 1;
+        // A boolean variable for checking if user wants to stop the program
+	boolean stop = false;
         try {
             aSocket = new DatagramSocket();
-            while(cont != 0){
-            System.out.println("Please enter your message:");
-            String msg = console.readLine();
-            byte[] m = msg.getBytes();
-            InetAddress aHost = InetAddress.getByName(IPAddress);
-            DatagramPacket request = new DatagramPacket(m, m.length, aHost, serverPort);
-            aSocket.send(request);
-            byte[] buffer = new byte[1000];
-            DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
-            aSocket.receive(reply);
-            System.out.println("Reply: " + new String(reply.getData()));
-            System.out.println("Do you want to continue? (input 0 for No, other number for Yes) : ");
-            cont = Integer.parseInt(console.readLine());
-        }
+            while(!stop) {
+		// Ask user for a message to send to the server
+		System.out.print("\nPlease enter your message:");
+		String msg = console.readLine();
+		// convert a string message into byte array
+		byte[] m = msg.getBytes();
+		// get the IP Address from string input earlier
+		InetAddress aHost = InetAddress.getByName(IPAddress);
+		// prepare a Datagram Packet with provided message, IP Address and port number
+		DatagramPacket request = new DatagramPacket(m, m.length, aHost, serverPort);
+		// send Datagram Packet to server
+		aSocket.send(request);
+		byte[] buffer = new byte[1000];
+		// prepare a Datagram Packet for receiving a reply from server
+		DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+		// receive the Datagram Packet that server replies
+		aSocket.receive(reply);
+		// print out server's reply
+		System.out.println("Reply from Server: " + new String(reply.getData()));
+		// Ask if user wants to stop the program
+		stop = checkStop();
+	    }
         } catch (SocketException e) {
             System.out.println("Socket: " + e.getMessage());
         } catch (IOException e) {
@@ -39,5 +55,100 @@ public class UDPClient {
             if (aSocket != null)
                 aSocket.close();
         }
+    }
+
+    // This method is used to get port number from user
+    // and check if it's valid or not
+    public static int checkPortNumber() {
+	boolean checker = false;
+	String serverPort = "";
+	// This loop will run until user inputs a valid port number
+	while(!checker) {
+	    // Ask user for a port number
+	    System.out.print("\nPlease enter the port number provided by Server: ");
+	    Console console = System.console();
+            serverPort = console.readLine();
+	    // check if the port number is valid or not
+	    try {
+		if(Integer.parseInt(serverPort) >= 0 && Integer.parseInt(serverPort) <= 65535) {
+		    checker = true;
+		}
+	    }
+	    catch (NumberFormatException e) { }
+	    // if the port number is invalid, show error message to user
+	    if(!checker) {
+		System.out.println("\nYou have entered an invalid port number. Please try again.");
+		System.out.println("Port number should be an integer from 0 to 65535.");
+	    }
+	}
+	// return the valid port number
+	return Integer.parseInt(serverPort);
+    }
+
+    // This method is used to get IP address from user
+    // and check if it's valid or not
+    public static String checkIpAddress() {
+	boolean checker = false;
+	int counter = 0;
+	String IPAddress = "";
+	// This loop will run until user inputs a valid IP Address
+	while(!checker) {
+	    // Ask user for an IP Address
+	    System.out.print("\nPlease enter the IP Address provided by Server: ");
+	    Console console = System.console();
+            IPAddress = console.readLine();
+	    String[] elements = IPAddress.split("\\.");
+	    // check if the IP Address is valid or not
+	    if(elements.length == 4) {
+		try {
+		    for(int i = 0; i < elements.length; i++) {
+			if(Integer.parseInt(elements[i]) >= 0 && Integer.parseInt(elements[i]) <= 255) {
+			    counter++;
+			}
+		    }
+		    if(counter == 4) {
+			checker = true;
+		    }
+		} catch (NumberFormatException e) { }
+	    }
+	    // if the IP Address is invalid, show error message to user
+	    if(!checker) {
+		counter = 0;
+		System.out.println("\nYou have entered an invalid IP address. Please try again.");
+		System.out.println("IP addres should be a string of 4 integers from 0 to 255 splitted by periods.");
+	    }
+	}
+	// return the valid IP Address
+	return IPAddress;
+    }
+
+    // This method is used to check if user wants to stop the program or not
+    // and check if user's choice is valid or not
+    public static boolean checkStop() {
+	boolean checker = false, stop = false;
+	String choice = "";
+	// This loop will run until user inputs a valid option
+	while(!checker) {
+	    // Ask user for an option
+	    System.out.print("\nDo you want to continue? (y/n): ");
+	    Console console = System.console();
+            choice = console.readLine();
+	    choice = choice.toLowerCase();
+	    // check if the option is valid or not
+	    if(choice.equals("y") || choice.equals("yes")) {
+		stop = false;
+		checker = true;
+	    }
+	    else if(choice.equals("n") || choice.equals("no")) {
+		stop = true;
+		checker = true;
+	    }
+	    // if the option is invalid, show error message to user
+	    else {
+		System.out.println("\nInvalid option. Please try again.");
+	    }
+	}
+	// return a valid option
+	return stop;
     }
 }
--- Socket Programming/UDPServer.java
+++ Socket Programming/UDPServer.java
@@ -1,35 +1,52 @@
+// Group 5: Tyler Ton, Dinh Luong
+// CECS 327 - Assignment 3
+// This is an UDP Server code provided by instructor
+// Modified by Dinh Luong and Tyler Ton
+
 import java.net.*;
 import java.io.*;
 import java.net.InetAddress; 
 import java.io.Console;
+
 public class UDPServer {
+
     public static void main(String args[]) {
 
         DatagramSocket aSocket = null;
         String host;
+	// get the IP Address from the device running this program
         try {
             host = InetAddress.getLocalHost().getHostAddress();
           }
           catch (UnknownHostException ex) {
             host = "unknown";
           }
-        System.out.println("This is a Server");
-        System.out.println("Server IP Address : " +  host); 
-        System.out.println("Please enter a port number:");
-        Console console = System.console();
-        int portNumber = Integer.parseInt(console.readLine());
+	// Show user this is a Server
+        System.out.println("\nThis is an Echo Server");
+	// Show user the server IP Address
+        System.out.println("Server IP Address : " +  host);
+	// Get the port number from user by calling checkPortNumber method
+        int portNumber = checkPortNumber();
+	// Tell user the port that the server is listening to
+	System.out.println("\nNow server is listening to port: " +  portNumber + "\n"); 
         int i = 0;
         try {
             aSocket = new DatagramSocket(portNumber);
             byte[] buffer = new byte[1000];      
             while (true) {
+		// prepare a Datagram Packet for receiving a message from client
                 DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+		// receive the Datagram Packet which contains client's message
                 aSocket.receive(request);
+		// prepare a Datagram Packet for sending a reply back to client
                 DatagramPacket reply = new DatagramPacket(request.getData(), request.getLength(), request.getAddress(),
                         request.getPort());
+		// send Datagram Packet to client
                 aSocket.send(reply);
+		// count the total message received from client
                 i++;
-                System.out.println("Message(s) processed : "+i);
+		// show user the total message received from client
+                System.out.println("Message(s) processed : "+ i);
             }
         } catch (SocketException e) {
             System.out.println("Socket: " + e.getMessage());
@@ -39,5 +56,33 @@ public class UDPServer {
             if (aSocket != null)
                 aSocket.close();
         }
+    }
+
+    // This method is used to get port number from user
+    // and check if it's valid or not
+    public static int checkPortNumber() {
+	boolean checker = false;
+	String serverPort = "";
+	// This loop will run until user inputs a valid port number
+	while(!checker) {
+	    // Ask user for a port number
+	    System.out.print("\nPlease enter a port number: ");
+	    Console console = System.console();
+            serverPort = console.readLine();
+	    // check if the port number is valid or not
+	    try {
+		if(Integer.parseInt(serverPort) >= 0 && Integer.parseInt(serverPort) <= 65535) {
+		    checker = true;
+		}
+	    }
+	    catch (NumberFormatException e) { }
+	    // if the port number is invalid, show error message to user
+	    if(!checker) {
+		System.out.println("\nYou have entered an invalid port number. Please try again.");
+		System.out.println("Port number should be an integer from 0 to 65535.");
+	    }
+	}
+	// return the valid port number
+	return Integer.parseInt(serverPort);
     }
 }
